4fad44282e41ae814e418d3aad5575456dd02dbd
Aleksandr Petryha <cazhqa666@gmail.com>
2020-05-16 09:44:51 +0300

    lesson 1 homework

--- script/script.js
+++ script/script.js
@@ -9,21 +9,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const playTest = () => {
         const renderQuestions = () => {
-            // questionTitle.textContent = `${question}`;
+        const name = "Стандарт";
+        const img = "./image/burger.png"
 
             formAnswers.innerHTML = `
                 <div class="answers-item d-flex flex-column">
                     <input type="radio" id="answerItem1" name="answer" class="d-none">
                     <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="./image/burger.png" alt="burger">
-                    <span>Стандарт</span>
+                    <img class="answerImg" src=${img} alt="burger">
+                    <span>${name}</span>
                     </label>
                 </div>
                 <div class="answers-item d-flex justify-content-center">
                 <input type="radio" id="answerItem2" name="answer" class="d-none">
                     <label for="answerItem2" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="./image/burgerBlack.png" alt="burger">
-                    <span>Черный</span>
+                    <img class="answerImg" src=${img} alt="burger">
+                    <span>${name}</span>
                     </label>
                     </div>
                     `
