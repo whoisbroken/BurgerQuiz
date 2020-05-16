f5081030cb3b525f4d7ebbcbde370b67f315775e
Aleksandr Petryha <cazhqa666@gmail.com>
2020-05-16 17:53:19 +0300

    lesson 2 homework

--- script/script.js
+++ script/script.js
@@ -5,31 +5,155 @@ document.addEventListener('DOMContentLoaded', function () {
     const questionTitle = document.querySelector('#question');
     const formAnswers = document.querySelector('#formAnswers');
     const burger = document.querySelector('.burger');
+    const nextButton = document.querySelector('#next')
+    const prevButton = document.querySelector('#prev');
 
+
+    const questions = [
+        {
+            question: "Какого цвета бургер?",
+            answers: [
+                {   
+                    id: 1,
+                    title: 'Стандарт',
+                    url: './image/burger.png'
+                },
+                {   
+                    id: 2,
+                    title: 'Черный',
+                    url: './image/burgerBlack.png'
+                }
+            ],
+            type: 'radio'
+        },
+        {
+            question: "Из какого мяса котлета?",
+            answers: [
+                {   
+                    id: 1,
+                    title: 'Курица',
+                    url: './image/chickenMeat.png'
+                },
+                {   
+                    id: 2,
+                    title: 'Говядина',
+                    url: './image/beefMeat.png'
+                },
+                {   
+                    id: 3,
+                    title: 'Свинина',
+                    url: './image/porkMeat.png'
+                }
+            ],
+            type: 'radio'
+        },
+        {
+            question: "Дополнительные ингредиенты?",
+            answers: [
+                {   
+                    id: 1,
+                    title: 'Помидор',
+                    url: './image/tomato.png'
+                },
+                {   
+                    id: 2,
+                    title: 'Огурец',
+                    url: './image/cucumber.png'
+                },
+                {   
+                    id: 3,
+                    title: 'Салат',
+                    url: './image/salad.png'
+                },
+                {   
+                    id: 4,
+                    title: 'Лук',
+                    url: './image/onion.png'
+                }
+            ],
+            type: 'checkbox'
+        },
+        {
+            question: "Добавить соус?",
+            answers: [
+                {   
+                    id: 1,
+                    title: 'Чесночный',
+                    url: './image/sauce1.png'
+                },
+                {   
+                    id: 2,
+                    title: 'Томатный',
+                    url: './image/sauce2.png'
+                },
+                {   
+                    id: 3,
+                    title: 'Горчичный',
+                    url: './image/sauce3.png'
+                }
+            ],
+            type: 'radio'
+        }
+    ];
 
     const playTest = () => {
-        const renderQuestions = () => {
-        const name = "Стандарт";
-        const img = "./image/burger.png"
 
-            formAnswers.innerHTML = `
-                <div class="answers-item d-flex flex-column">
-                    <input type="radio" id="answerItem1" name="answer" class="d-none">
-                    <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src=${img} alt="burger">
-                    <span>${name}</span>
-                    </label>
-                </div>
-                <div class="answers-item d-flex justify-content-center">
-                <input type="radio" id="answerItem2" name="answer" class="d-none">
-                    <label for="answerItem2" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src=${img} alt="burger">
-                    <span>${name}</span>
-                    </label>
-                    </div>
-                    `
+        let numberQuestion = 0;
+        
+        
+        const renderAnswers = (index) => {
+            console.log(numberQuestion);
+            
+            if (numberQuestion === 0) {
+                prevButton.style.display = 'none';
+            } 
+            
+            if (numberQuestion > 0) {
+                prevButton.style.display = 'block';
+            } 
+
+            if (numberQuestion === questions.length - 1) {
+                nextButton.style.display = 'none';
+            }
+
+            questions[index].answers.forEach((answer) => {
+
+                const answerItem = document.createElement('div');
+
+                answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+
+                answerItem.innerHTML = `
+                            <input type="${questions[index].type}" id="${answer.id}" name="answer" class="d-none">
+                            <label for="${answer.id}" class="d-flex flex-column justify-content-between">
+                            <img class="answerImg" src=${answer.url} alt="burger">
+                            <span>${answer.title}</span>
+                            </label>
+                            `
+                formAnswers.appendChild(answerItem);
+            })
         }
-        renderQuestions();
+
+        const renderQuestions = (indexQuestion) => {
+
+            
+            formAnswers.textContent = '';
+
+            questionTitle.textContent = `${questions[indexQuestion].question}`
+
+            renderAnswers(indexQuestion);
+        }
+        renderQuestions(numberQuestion);
+        
+
+        nextButton.onclick = () => {
+            numberQuestion++;
+            renderQuestions(numberQuestion);
+        }
+
+        prevButton.onclick = () => {
+            numberQuestion--;
+            renderQuestions(numberQuestions);
+        }
     }
 
 
@@ -48,4 +172,6 @@ document.addEventListener('DOMContentLoaded', function () {
         modalBlock.classList.remove('d-block');
         burger.classList.remove('active');
     })
+
+
 })
